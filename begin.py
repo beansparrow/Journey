536fa5c42d6e8f866a5064b77ee3dd24288123c7
Asparique <beansparrow@users.noreply.github.com>
2015-08-15 17:55:23 -0400

    A short game.
    A new update that shows what-if statements and how they can be really fun.

--- begin.py
+++ begin.py
@@ -1,7 +1,27 @@
-print "Hello! Im NICE bot created by Jai on 7/16/2015"
+print "Welcome to 'A Simple Life', Created by Jai Yarlagadda Sat Aug 15 2015" 
 
-print "What is your name?"
+print "You wake up and find yourself relaxed \nfrom a good sleep last night. You get a text message."
+print "Type 1 to check your phone"
+print "Type 2 to not check your phone" 
 
-name = raw_input('>')
+phone = raw_input('>')
 
-print "Hello, %s" %name 
+if phone == '1':
+	print "It is from your ex wife. It says, 'Hey, you left some stuff at my house, can you come and get them?'" 
+	print "Type 1 to go pick up your stuff" 
+	print "Type 2 to ignore the message" 
+	
+	stuff = raw_input('>')
+	if stuff == '1':
+		print "You drive to the house but get into a car accident. You die." 
+		
+	else:
+		print "You go back to sleep. Good night."
+	
+
+elif phone == '2':
+	print "You go back to sleep. Good night." 
+	
+else: 
+	print "You get a heart attack. You die." 
+	
